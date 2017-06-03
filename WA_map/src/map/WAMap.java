b6331ab3c066e1baeead74f8ec266c7e9fd0a745
DESKTOP-A917PJG\Hugo <hugo.rous@gmail.com>
2017-06-04 01:07:32 +0200

    better demo

--- WA_map/src/map/WAMap.java
+++ WA_map/src/map/WAMap.java
@@ -1,6 +1,8 @@
 package map;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -8,9 +10,40 @@ import javax.swing.JPanel;
 import island.Island;
 
 public class WAMap {
+	private List<Island> islands = new ArrayList<Island>();
 	
 	public WAMap() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	private void setTestIsland(){
+		Island ref = new Island("ref", 250.0,  250.0);
+		ref.setIslandSize(30);
+		
+		Island t1 = new Island("t1", ref, 3.14, 50);
+		t1.setColor(Color.GREEN);
+		t1.setIslandSize(15);
+		Island t11 = new Island("t11", t1, 2, 100);
+		t11.setColor(Color.GREEN);
+		Island t12 = new Island("t12", t1, 4, 50);
+		t12.setColor(Color.GREEN);
+		
+		Island t2 = new Island("t2", ref, 0, 50);
+		t2.setColor(Color.YELLOW);
+		t2.setIslandSize(15);
+		Island t21 = new Island("t21", t2, 1, 50);
+		t21.setColor(Color.RED);
+		Island t22 = new Island("t22", t2, 5, 100);
+		t22.setColor(Color.RED);
+		
+		islands.add(ref);
+		islands.add(t1);
+		islands.add(t2);
+		islands.add(t11);
+		islands.add(t12);
+		islands.add(t21);
+		islands.add(t22);
+		
 	}
 	
 	public void draw(){
@@ -22,18 +55,16 @@ public class WAMap {
 	    
 	    JPanel panel = new JPanel();
 	    panel.setLayout(null);
-	    Island ref = new Island("test", 250.0,  250.0);
-	    ref.setIslandSize(15);
-	    panel.add(ref);
-	    panel.add(new Island("testFromRef", ref, 1.57, 50));
-	    panel.add(new Island("testFromRef", ref, 3.14, 50));
-	    panel.add(new Island("testFromRef", ref, 0, 50));
-	    panel.add(new Island("testFromRef", ref, 4.70, 50));
+	    setTestIsland();
+	    for(Island i : islands){
+	    	panel.add(i);
+	    }
 	    //panel.setBackground(Color.ORANGE);
 	    window.setContentPane(panel);
 	    //window.pack();
 	    
 	    window.setVisible(true);
+	    window.repaint();
 	}
 
 }
